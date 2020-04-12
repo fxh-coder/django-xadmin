5560fb2db6eee4c036deacd570fb9d6d5848b057
fxh <3314965946@qq.com>
2020-04-12 23:17:41 +0800

    完成讲师显示、分页、人气排名、排行榜、详情功能

--- apps/organizations/urls.py
+++ apps/organizations/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from organizations.views import OrgView, AddAskView, OrgHomeView, OrgTeacherView, OrgCourseView, OrgDescView
+from organizations.views import OrgView, AddAskView, OrgHomeView, OrgTeacherView, OrgCourseView, OrgDescView, TeacherListView, TeacherDetailView
 
 urlpatterns = [
     url(r'^list/$', OrgView.as_view(), name="list"),
@@ -8,4 +8,8 @@ urlpatterns = [
     url(r'^(?P<org_id>\d+)/teacher/$', OrgTeacherView.as_view(), name="teacher"),
     url(r'^(?P<org_id>\d+)/course/$', OrgCourseView.as_view(), name="course"),
     url(r'^(?P<org_id>\d+)/desc/$', OrgDescView.as_view(), name="desc"),
+
+    # 讲师列表页
+    url(r'^teachers/$', TeacherListView.as_view(), name="teachers"),
+    url(r'^teachers/(?P<teacher_id>\d+)/$', TeacherDetailView.as_view(), name="teacher_detail"),
 ]
